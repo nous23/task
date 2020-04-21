3b95bda019b685ea7ee018f3684af2ce8d0c943a
nous23 <cchen23@outlook.com>
2020-04-21 17:10:05 +0800

    Update main.go

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/sirupsen/logrus"
 
@@ -16,7 +17,11 @@ func init() {
 
 func initLog() {
 	if !util.Exist(config.AppConfigs.LogPath) {
-		_, err := os.Create(config.AppConfigs.LogPath)
+		err := os.MkdirAll(filepath.Dir(config.AppConfigs.LogPath), os.ModePerm)
+		if err != nil {
+			log.Panicf("mkdir %s failed: %v", filepath.Dir(config.AppConfigs.LogPath), err)
+		}
+		_, err = os.Create(config.AppConfigs.LogPath)
 		if err != nil {
 			log.Panicf("create log file %s failed: %v", config.AppConfigs.LogPath, err)
 		}
