f65b7e522c770e600ba7a2282a7bf8375fe820ef
nous23 <cchen23@outlook.com>
2020-04-21 17:10:57 +0800

    Update config.go

--- src/pkg/config/config.go
+++ src/pkg/config/config.go
@@ -1,15 +1,19 @@
 package config
 
 import (
-	"encoding/json"
+	"bytes"
 	"io/ioutil"
 	"log"
+	"os/user"
+	"path/filepath"
+	"text/template"
 
+	"gopkg.in/yaml.v2"
 	"task/pkg/global"
 )
 
 type AppConfig struct {
-	LogPath string `json:"log_path"`
+	LogPath string `yaml:"log_path"`
 }
 
 var AppConfigs AppConfig
@@ -19,8 +23,31 @@ func init() {
 	if err != nil {
 		log.Panicf("read %s failed: %v", global.AppConfigPath, err)
 	}
-
-	if err = json.Unmarshal(b, &AppConfigs); err != nil {
+	if err = yaml.Unmarshal(refreshAppConfig(b), &AppConfigs); err != nil {
+		log.Println(string(refreshAppConfig(b)))
 		log.Panicf("unmarshal app config failed: %v", err)
 	}
+}
+
+func refreshAppConfig(data []byte) []byte {
+	text := string(data)
+	u, err := user.Current()
+	if err != nil {
+		log.Panicf("get current user failed: %v", err)
+	}
+
+	m := map[string]string{
+		"user_home_dir": filepath.Clean(u.HomeDir),
+	}
+
+	tmpl, err := template.New("var").Parse(text)
+	if err != nil {
+		log.Panicf("parse template %s failed: %v", text, err)
+	}
+	var b bytes.Buffer
+	err = tmpl.Execute(&b, m)
+	if err != nil {
+		log.Panicf("exec template failed: %v", err)
+	}
+	return b.Bytes()
 }
