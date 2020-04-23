64b6549ef21d8e7d6320110cf76e385b66b9e504
nous23 <cchen23@outlook.com>
2020-04-23 20:47:52 +0800

    Update controller.go

--- src/apiserver/controller/controller.go
+++ src/apiserver/controller/controller.go
@@ -3,6 +3,9 @@ package controller
 import (
 	"net/http"
 
+	log "github.com/sirupsen/logrus"
+
+	"task/apiserver/model"
 	"task/global"
 
 	"github.com/gin-gonic/gin"
@@ -14,4 +17,15 @@ func Hello(c *gin.Context) {
 
 func TaskHome(c *gin.Context) {
 	c.HTML(http.StatusOK, global.TaskHTMLFile, nil)
+}
+
+func ListTask(c *gin.Context) {
+	tasks, err := model.ListTask()
+	if err != nil {
+		log.Errorf("list task failed: %v", err)
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, tasks)
 }
