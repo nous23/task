85a2ba9d6fb103c15f3d1b2ea2d2e9736bc8ce56
nous23 <cchen23@outlook.com>
2020-04-24 17:40:18 +0800

    Update controller.go

--- src/apiserver/controller/controller.go
+++ src/apiserver/controller/controller.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 
 	log "github.com/sirupsen/logrus"
@@ -28,4 +30,51 @@ func ListTask(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, tasks)
+}
+
+func GetTask(c *gin.Context) {
+	id := c.Param("id")
+	task, err := model.GetTask(id)
+	if err != nil {
+		log.Errorf("get task %s failed: %v", id, err)
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, task)
+}
+
+func UpdateTask(c *gin.Context) {
+	b, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		log.Errorf("read body failed: %v", err)
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+	log.Tracef("update body: %s", string(b))
+	update := make(map[string]interface{})
+	err = json.Unmarshal(b, &update)
+	if err != nil {
+		log.Errorf("unmarshal json failed: %v", err)
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+	update["id"] = c.Param("id")
+	err = model.UpdateTask(update)
+	if err != nil {
+		log.Errorf("update task %s failed: %v", update["id"], err)
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.Status(http.StatusOK)
+}
+
+func DeleteTask(c *gin.Context) {
+	id := c.Param("id")
+	err := model.DeleteTask(id)
+	if err != nil {
+		log.Errorf("delete task %s failed: %v", id, err)
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.Status(http.StatusOK)
 }
