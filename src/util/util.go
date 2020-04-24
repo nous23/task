b3a286e59dd24340e588b06e09a393a19c6a00ac
nous23 <cchen23@outlook.com>
2020-04-24 17:43:23 +0800

    Update util.go

--- src/util/util.go
+++ src/util/util.go
@@ -1,6 +1,9 @@
 package util
 
-import "os"
+import (
+	"os"
+	"strings"
+)
 
 // report whether path exist
 func Exist(path string) bool {
@@ -9,4 +12,19 @@ func Exist(path string) bool {
 		return false
 	}
 	return true
+}
+
+func DeleteLast(s, d string) string {
+	index := strings.LastIndex(s, d)
+	if index == -1 {
+		return s
+	}
+	return s[:index] + s[index+1:]
+}
+
+func HasKey(m map[string]interface{}, key string) bool {
+	if _, ok := m[key]; ok {
+		return true
+	}
+	return false
 }
