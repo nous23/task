ba209f8f1bc38f0869b19033e7ca4add9721275d
nous23 <cchen23@outlook.com>
2020-04-29 20:19:37 +0800

    Update main.go

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -6,21 +6,13 @@ import (
 	log "github.com/sirupsen/logrus"
 
 	"task/apiserver"
-	"task/database"
 	_ "task/log"
 )
 
 func main() {
 	log.Info("========== task start ==========")
 
-	var err error
-	err = database.InitDB()
-	if err != nil {
-		log.Errorf("init db failed: %v", err)
-		os.Exit(1)
-	}
-
-	err = apiserver.Run()
+	err := apiserver.Run()
 	if err != nil {
 		log.Errorf("start apiserver failed: %v", err)
 		os.Exit(1)
