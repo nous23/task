d05d2dff372b3078ecf94db0c02c935b55ee9490
nous23 <cchen23@outlook.com>
2020-07-10 16:42:07 +0800

    Update model.go

--- src/apiserver/model/model.go
+++ src/apiserver/model/model.go
@@ -1,248 +1,194 @@
 package model
 
 import (
+	"bytes"
 	"fmt"
+	"text/template"
 
 	log "github.com/sirupsen/logrus"
 
 	"task/database"
+	"task/util"
 )
 
-type Task struct {
-	Id        int    `json:"id"`
-	Completed bool   `json:"completed"`
-	Title     string `json:"title"`
-	Type      string `json:"type"`
-	Detail    string `json:"detail"`
-	StartTime string `json:"start_time"`
-	EndTime   string `json:"end_time"`
-	Deadline  string `json:"deadline"`
+type sqlBuilderEnum int
+
+// operation enum
+const (
+	listTask sqlBuilderEnum = iota
+	getTask
+	updateTask
+	deleteTask
+	createTask
+	createSubTask
+	listSubTask
+	deleteSubTask
+	updateSubTask
+	register
+	getUserByName
+)
+
+var sqlTemplate = map[sqlBuilderEnum]string{
+	listTask: `select * from task
+{{- if not (isEmpty .)}}
+where
+{{- if hasKey . "complete"}}
+completed = {{.complete}}
+{{- end}}
+{{- end}};`,
+	getTask: `select * from task where id={{.id}};`,
+	updateTask: `update task set
+{{ if hasKey . "title" }}
+title = '{{.title}},'
+{{ end }}
+{{ if hasKey . "completed" }}
+completed = {{.completed}},
+{{end}}
+{{if hasKey . "type"}}
+task_type = '{{.type}}',
+{{end}}
+{{if hasKey . "detail"}}
+detail = '{{.detail}}',
+{{end}}
+{{if hasKey . "start_time"}}
+start_time = '{{.start_time}}',
+{{end}}
+{{if hasKey . "end_time"}}
+end_time = '{{.end_time}}',
+{{end}}
+{{if hasKey . "deadline"}}
+deadline = '{{.deadline}}',
+{{end}}
+where id = {{.id}};`,
+	deleteTask: `delete from task where id={{.id}};`,
+	createTask: `insert into task (completed, title, task_type, detail, start_time, deadline) values
+({{.completed}}, '{{.title}}', '{{.type}}', '{{.detail}}', '{{.start_time}}', '{{.deadline}}');`,
+	createSubTask: `insert into subtask (task_id, title) values ({{.task_id}}, '{{.title}}');`,
+	listSubTask:   `select * from subtask where task_id={{.task_id}} order by id;`,
+	deleteSubTask: `delete from subtask where id={{.id}};`,
+	updateSubTask: `update subtask set
+{{if hasKey . "title"}}
+title='{{.title}}',
+{{end}}
+{{if hasKey . "completed"}}
+completed={{.completed}},
+{{end}}
+where id={{.id}};`,
+	register:      `insert into users (user_name, user_password) values ('{{.username}}', '{{.password}}');`,
+	getUserByName: `select * from users where user_name='{{.username}}';`,
 }
 
-func ListTask(p Params) ([]*Task, error) {
-	log.Trace("start list task")
-	sqlCmd, err := buildSql(listTask, p)
-	if err != nil {
-		log.Errorf("build list task sql failed: %v", err)
-		return nil, err
-	}
-	rows, err := database.Get().Query(sqlCmd)
-	if err != nil {
-		log.Errorf("exec sql %s failed: %v", sqlCmd, err)
-		return nil, err
-	}
-	var tasks []*Task
-	for rows.Next() {
-		t := &Task{}
-		err = rows.Scan(&t.Id, &t.Completed, &t.Title, &t.Type, &t.Detail, &t.StartTime, &t.EndTime, &t.Deadline)
-		if err != nil {
-			log.Errorf("scan query results failed: %v", err)
-			return nil, err
-		}
-		tasks = append(tasks, t)
-	}
-	return tasks, nil
+var hooks = map[sqlBuilderEnum]func(string) string{
+	updateTask: func(sql string) string {
+		return util.DeleteLast(sql, ",")
+	},
+	updateSubTask: func(sql string) string {
+		return util.DeleteLast(sql, ",")
+	},
 }
 
-func GetTask(id string) ([]*Task, error) {
-	log.Tracef("start get task %s", id)
-	sqlCmd, err := buildSql(getTask, Params{"id": id})
-	if err != nil {
-		log.Errorf("build get task sql failed: %v", err)
-		return nil, err
-	}
-	rows, err := database.Get().Query(sqlCmd)
-	if err != nil {
-		log.Errorf("exec sql %s failed: %v", sqlCmd, err)
-		return nil, err
-	}
-	var ts []*Task
-	for rows.Next() {
-		t := &Task{}
-		err = rows.Scan(&t.Id, &t.Completed, &t.Title, &t.Type, &t.Detail, &t.StartTime, &t.EndTime, &t.Deadline)
-		if err != nil {
-			log.Errorf("scan query results failed: %v", err)
-			return nil, err
-		}
-		ts = append(ts, t)
-	}
-	return ts, nil
+var sqlBuilders = map[sqlBuilderEnum]*sqlBuilder{
+	listTask:      newSqlBuilder(listTask),
+	getTask:       newSqlBuilder(getTask),
+	updateTask:    newSqlBuilder(updateTask),
+	deleteTask:    newSqlBuilder(deleteTask),
+	createTask:    newSqlBuilder(createTask),
+	createSubTask: newSqlBuilder(createSubTask),
+	listSubTask:   newSqlBuilder(listSubTask),
+	deleteSubTask: newSqlBuilder(deleteSubTask),
+	updateSubTask: newSqlBuilder(updateSubTask),
+	register:      newSqlBuilder(register),
+	getUserByName: newSqlBuilder(getUserByName),
 }
 
-func UpdateTask(value map[string]interface{}) error {
-	log.Tracef("start update task with value: %v", value)
-	sqlCmd, err := buildSql(updateTask, value)
-	if err != nil {
-		log.Errorf("build sql for update task failed: %v", err)
-		return err
-	}
-	_, err = database.Get().Exec(sqlCmd)
-	if err != nil {
-		log.Errorf("exec sql %s failed: %v", sqlCmd, err)
-		return err
-	}
-	return nil
+type Params map[string]interface{}
+
+const (
+	errorSqlBuilderNotFound string = "can not find sql builder"
+)
+
+type sqlBuilder struct {
+	tmpl string
+	hook func(string) string
 }
 
-func DeleteTask(id string) error {
-	log.Tracef("start delete task %s", id)
-	sqlCmd, err := buildSql(deleteTask, Params{"id": id})
-	if err != nil {
-		log.Errorf("build sql for delete task failed: %v", err)
-		return err
+func (b *sqlBuilder) build(p Params) (string, error) {
+	if p == nil {
+		return b.tmpl, nil
 	}
-	_, err = database.Get().Exec(sqlCmd)
+	t, err := template.New("sql").Funcs(template.FuncMap{
+		"hasKey":  util.HasKey,
+		"isEmpty": util.IsEmpty,
+	}).Parse(b.tmpl)
 	if err != nil {
-		log.Errorf("exec sql %s failed: %v", sqlCmd, err)
-		return err
+		return "", err
 	}
-	return nil
+	var buffer bytes.Buffer
+	err = t.Execute(&buffer, p)
+	if err != nil {
+		return "", err
+	}
+	if b.hook != nil {
+		return b.hook(buffer.String()), nil
+	}
+	return buffer.String(), nil
 }
 
-func CreateTask(p Params) error {
-	log.Tracef("start create task: %v", p)
-	sqlCmd, err := buildSql(createTask, p)
-	if err != nil {
-		log.Errorf("build sql for delete task failed: %v", err)
-		return err
-	}
-	_, err = database.Get().Exec(sqlCmd)
-	if err != nil {
-		log.Errorf("exec sql %s failed: %v", sqlCmd, err)
-		return err
-	}
-	return nil
-}
-
-func CreateSubTask(p Params) error {
-	log.Tracef("start create sub task: %v", p)
-	sqlCmd, err := buildSql(createSubTask, p)
-	if err != nil {
-		log.Errorf("build sql for create sub task failed: %v", err)
-		return err
-	}
-	_, err = database.Get().Exec(sqlCmd)
-	if err != nil {
-		log.Errorf("exec sql [%s] failed: %v", sqlCmd, err)
-		return err
-	}
-	return nil
-}
-
-type SubTask struct {
-	Id        int    `json:"id"`
-	TaskId    int    `json:"task_id"`
-	Title     string `json:"title"`
-	Completed bool   `json:"completed"`
-}
-
-func ListSubTask(taskId string) ([]*SubTask, error) {
-	log.Trace("start list sub task")
-	sqlCmd, err := buildSql(listSubTask, Params{"task_id": taskId})
-	if err != nil {
-		log.Errorf("build list sub task sql failed: %v", err)
-		return nil, err
-	}
-	rows, err := database.Get().Query(sqlCmd)
-	if err != nil {
-		log.Errorf("exec sql [%s] failed: %v", sqlCmd, err)
-		return nil, err
-	}
-	var subTasks []*SubTask
-	for rows.Next() {
-		st := &SubTask{}
-		err = rows.Scan(&st.Id, &st.TaskId, &st.Title, &st.Completed)
-		if err != nil {
-			log.Errorf("scan list sub task query results failed: %v", err)
-			return nil, err
-		}
-		subTasks = append(subTasks, st)
-	}
-	return subTasks, nil
-}
-
-func DeleteSubTask(id string) error {
-	log.Tracef("start delete sub task %s", id)
-	sqlCmd, err := buildSql(deleteSubTask, Params{"id": id})
-	if err != nil {
-		log.Errorf("build sql for delete sub task failed: %v", err)
-		return err
-	}
-	_, err = database.Get().Exec(sqlCmd)
-	if err != nil {
-		log.Errorf("exec sql [%s] failed: %v", sqlCmd, err)
-		return err
-	}
-	log.Tracef("exec [%s] success", sqlCmd)
-	return nil
-}
-
-func UpdateSubTask(p Params) error {
-	id, ok := p["id"]
+func newSqlBuilder(e sqlBuilderEnum) *sqlBuilder {
+	tmpl := sqlTemplate[e]
+	hook, ok := hooks[e]
 	if !ok {
-		msg := fmt.Sprintf("sub task id not specified: %v", p)
-		log.Error(msg)
-		return fmt.Errorf(msg)
+		hook = nil
 	}
-	log.Tracef("start update sub task %s", id)
-	sqlCmd, err := buildSql(updateSubTask, p)
-	if err != nil {
-		log.Errorf("build sql for update sub task failed: %v", err)
-		return err
+	return &sqlBuilder{
+		tmpl: tmpl,
+		hook: hook,
 	}
-	_, err = database.Get().Exec(sqlCmd)
-	if err != nil {
-		log.Errorf("exec sql [%s] failed: %v", sqlCmd, err)
-		return err
-	}
-	return nil
 }
 
-func Register(p Params) error {
-	log.Tracef("start register user: ", p["username"])
-	sqlCmd, err := buildSql(register, p)
-	if err != nil {
-		log.Errorf("build sql for register failed: %v", err)
-		return err
+func buildSql(e sqlBuilderEnum, p Params) (string, error) {
+	sb, ok := sqlBuilders[e]
+	if !ok {
+		return "", fmt.Errorf(errorSqlBuilderNotFound)
 	}
-	_, err = database.Get().Exec(sqlCmd)
+	sqlCmd, err := sb.build(p)
 	if err != nil {
-		log.Errorf("exec sql [%s] failed: %v", sqlCmd, err)
-		return err
+		return "", err
 	}
-	return nil
+	return sqlCmd, nil
 }
 
-type User struct {
-	Id int
-	Name string
-	Password string
-}
+const (
+	createTaskSql = `create table if not exists task (
+id int primary key auto_increment not null,
+completed bool default false not null,
+title varchar(256) default 'untitled' not null,
+task_type varchar(32) default 'unknown' not null,
+detail varchar(1024) default 'no detail' not null,
+start_time datetime default current_timestamp not null,
+end_time datetime default current_timestamp not null,
+deadline datetime default current_timestamp not null
+);`
+	createSubTaskSql = `create table if not exists subtask (
+id int primary key not null auto_increment,
+task_id int not null,
+index task_id_index (task_id),
+title varchar(256) default 'untitled' not null,
+completed bool default false not null,
+foreign key (task_id) references task(id) on delete cascade on update cascade);`
+	createUsersSql = `create table if not exists users (
+id int primary key auto_increment,
+user_name varchar(32) not null default '',
+user_password varchar(64) not null
+);`
+)
 
-func GetUserByName(username string) ([]*User, error) {
-	log.Tracef("start get user by name: %s", username)
-	p := Params{
-		"username": username,
-	}
-	sqlCmd, err := buildSql(getUserByName, p)
-	if err != nil {
-		log.Errorf("build sql for get user by name failed: %v", err)
-		return nil, err
-	}
-	rows, err := database.Get().Query(sqlCmd)
-	if err != nil {
-		log.Errorf("exec sql [%s] failed: %v", sqlCmd, err)
-		return nil, err
-	}
-	var users []*User
-	for rows.Next() {
-		u := &User{}
-		err = rows.Scan(&u.Id, &u.Name, &u.Password)
+func init() {
+	createSqls := []string{createTaskSql, createSubTaskSql, createUsersSql}
+	for _, sqlCmd := range createSqls {
+		_, err := database.Get().Exec(sqlCmd)
 		if err != nil {
-			log.Errorf("scan get user by name query results failed: %v", err)
-			return nil, err
+			log.Panicf("exec sql [%s] failed: %v", sqlCmd, err)
 		}
-		users = append(users, u)
 	}
-	return users, nil
 }
